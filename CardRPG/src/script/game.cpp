ef0aa9f6dcd51643e75581d8fa6094155b07fb30
pupungeggang2 <kyongkyong2892@gmail.com>
2025-09-02 23:18:29 +0900

    Upscaling

--- CardRPG/src/script/game.cpp
+++ CardRPG/src/script/game.cpp
@@ -2,8 +2,19 @@
 
 Game::Game() {
     sf::err().rdbuf(NULL);
-    window = sf::RenderWindow(sf::VideoMode({1280u, 720u}), "Adventure Game");
+    unsigned int width, height;
+    sf::VideoMode videoMode = sf::VideoMode::getDesktopMode();
+    if (videoMode.size.x > 2000) {
+        width = 1920; height = 1080;
+    } else {
+        width = 1280; height = 720;
+    }
+
+    window = sf::RenderWindow(sf::VideoMode({width, height}), "Adventure Game");
     window.setFramerateLimit(60);
+    sf::View view({640, 360}, {1280, 720});
+    window.setView(view);
+
     tex.insert(std::make_pair("test", sf::Texture("image/test.png")));
     s.setTexture(tex.at("test"));
     s.setTextureRect(sf::IntRect({0, 0}, {80, 80}));
