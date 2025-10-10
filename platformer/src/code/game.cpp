cc7eae392e06df5488acf4be3199e06a6e689d88
pupungeggang2 <kyongkyong2892@gmail.com>
2025-10-11 02:35:56 +0900

    Scale

--- platformer/src/code/game.cpp
+++ platformer/src/code/game.cpp
@@ -4,10 +4,12 @@
 Game::Game() {
     sf::err().rdbuf(NULL);
     sf::VideoMode videoMode = sf::VideoMode::getDesktopMode();
-    if (videoMode.size.x > 2000) {
+    if (videoMode.size.x > 2560) {
+        width = 2560; height = 1440;
+    } else if (videoMode.size.x > 2000) {
         width = 1920; height = 1080;
     } else {
-        width = 1280; height = 720;
+        width = 1080; height = 720;
     }
 
     window = sf::RenderWindow(sf::VideoMode({width, height}), "Auto Card Battle");
