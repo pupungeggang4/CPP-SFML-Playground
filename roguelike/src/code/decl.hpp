dbdf2dfcd6751979baee97a5c4b3cc49c40e134d
pupungeggang2 <kyongkyong2892@gmail.com>
2025-10-12 00:43:23 +0900

    Temp

--- roguelike/src/code/decl.hpp
+++ roguelike/src/code/decl.hpp
@@ -10,11 +10,25 @@ class Card;
 
 class Render;
 class Func;
+
+class SceneTitle;
+class SceneVillage;
+class SceneBattle;
+
 class Game;
 
 class UI {
     public:
         static std::unordered_map<std::string, std::vector<int>> title;
+        static std::unordered_map<std::string, std::vector<int>> village;
+        static std::unordered_map<std::string, std::vector<int>> battle;
+        static std::unordered_map<std::string, std::vector<int>> menu;
+};
+
+class Render {
+    public:
+        static void renderText(sf::RenderWindow& window, sf::Text rText, std::string text, std::vector<float> pos);
+        static void strokeRect(sf::RenderWindow& window, sf::RectangleShape rRect, std::vector<float> rect);
 };
 
 class Func {
@@ -22,10 +36,23 @@ class Func {
         static bool pointInsideRectUI(sf::Vector2i pos, sf::IntRect rect);
 };
 
+class SceneTitle {
+
+};
+
+class SceneVillage {
+
+};
+
+class SceneBattle {
+    
+};
+
 class Game {
     public:
         sf::RenderWindow window;
         unsigned int width, height;
+        sf::Text rText; sf::RectangleShape rRect; sf::Font neodgm;
         Game();
         void run(shared_ptr<Game> game);
         void handleScene(shared_ptr<Game> game);
--- roguelike/src/code/render.cpp
+++ roguelike/src/code/render.cpp
@@ -1,3 +1,11 @@
 #pragma once
 #include "general.hpp"
 #include "decl.hpp"
+
+void Render::renderText(sf::RenderWindow& window, sf::Text rText, std::string text, std::vector<float> pos) {
+
+}
+
+void Render::strokeRect(sf::RenderWindow& window, sf::RectangleShape rRect, std::vector<float> rect) {
+
+}
