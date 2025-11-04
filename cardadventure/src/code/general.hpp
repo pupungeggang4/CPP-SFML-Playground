f4f6ae0d78b8fa4e7860134e869f57443c6b0bbe
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-05 00:13:25 +0900

    Temp

---
+++ cardadventure/src/code/general.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <array>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+
+#include <memory>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+using std::make_shared;
+using std::shared_ptr;
---
+++ cardadventure/src/main.cpp
@@ -0,0 +1,23 @@
+#include "code/general.hpp"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::err().rdbuf(NULL);
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
