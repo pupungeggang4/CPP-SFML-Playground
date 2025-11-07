9402997cf9892ef747f00ee34170025afc9dce67
pupungeggang2 <kyongkyong2892@gmail.com>
2025-11-07 13:55:56 +0900

    Temp

--- physics2d/src/code/general.hpp
+++ physics2d/src/code/general.hpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+#include <memory>
 
 #include "SFML/Graphics.hpp"
 #include "SFML/Window.hpp"
 #include "SFML/System.hpp"
+
+using std::shared_ptr;
+using std::make_shared;
